8403df6e425c26fc21b089677d294d76fafc7c5d
Ranjuna120 <IT22236296@my.sliit.lk>
2025-09-08 22:51:19 +0530

    Create migration.sql

---
+++ prisma/migrations/20250908171205_add_meal/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Meal" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "calories" INTEGER NOT NULL,
+    "protein" REAL NOT NULL,
+    "carbs" REAL NOT NULL,
+    "fats" REAL NOT NULL,
+    "consumed_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "Meal_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE INDEX "Meal_user_id_consumed_at_idx" ON "Meal"("user_id", "consumed_at");
